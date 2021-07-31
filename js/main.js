7cac90c320af2dcc7941a8aec0b137c4386fe2ab
Andjelk01 <nemanja11andjelkovic@gmail.com>
2021-07-31 14:57:45 +0200

    Dodat JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function izracunajTip(tips)
+{   
+   var bill=document.getElementById("billVal").value;
+   var people=document.getElementById("peopleVal").value;
+   var tip=tips.value;
+
+    if(!(isNaN(bill) && isNaN(people)))
+    {
+      document.getElementById("tipamount").innerHTML=((((bill*tip)/100)/people).toFixed(2).toString()+" $");
+      document.getElementById("total").innerHTML=((((parseInt(((bill*tip)/100)))+parseInt(bill))/people).toFixed(2).toString()+" $");
+    }  
+}
+
+function resetbutton()
+{
+  document.getElementById("tipamount").innerHTML="0,00 $";
+  document.getElementById("total").innerHTML="0,00 $";
+}
