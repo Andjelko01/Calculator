19a8c3c0e3c1f41e1e69c0fe9a90b7d077c12bf3
Andjelk01 <nemanja11andjelkovic@gmail.com>
2021-08-04 17:01:02 +0200

    V 1.0

--- js/main.js
+++ js/main.js
@@ -2,17 +2,20 @@ function izracunajTip(tips)
 {   
    var bill=document.getElementById("billVal").value;
    var people=document.getElementById("peopleVal").value;
-   var tip=tips.value;
+   var test=tips.value;
+
+   var tip=Number(tips.value.replace('%',''));
+   console.log(tip);
 
     if(!(isNaN(bill) && isNaN(people)))
     {
-      document.getElementById("tipamount").innerHTML=((((bill*tip)/100)/people).toFixed(2).toString()+" $");
-      document.getElementById("total").innerHTML=((((parseInt(((bill*tip)/100)))+parseInt(bill))/people).toFixed(2).toString()+" $");
-    }  
+      document.getElementById("tipamount").innerHTML=("$"+((((bill*tip)/100)/people).toFixed(2).toString()));
+      document.getElementById("total").innerHTML=("$"+((((parseInt(((bill*tip)/100)))+parseInt(bill))/people).toFixed(2).toString()));
+    } 
 }
 
 function resetbutton()
 {
-  document.getElementById("tipamount").innerHTML="0,00 $";
-  document.getElementById("total").innerHTML="0,00 $";
+  document.getElementById("tipamount").innerHTML="$0,00";
+  document.getElementById("total").innerHTML="$0,00";
 }
